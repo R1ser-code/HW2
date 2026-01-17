35809cf22b2ad5c7f93026515e0f8b5ffbdfb8da
R1ser-code <drynkin12@gmail.com>
2026-01-17 14:16:19 +0300

    Add files via upload

---
+++ дз со звездочкой.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+int main()
+{
+	std::string word;
+	std::cin >> word;
+	std::cout << "Your word:" << word << std::endl;
+	std::cout << "My word: " << word << std::endl;
+	return EXIT_SUCCESS;
+}
