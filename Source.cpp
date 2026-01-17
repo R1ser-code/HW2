a4726f425a5d8ad00a1cf4f3d7997c254b6c5f0f
R1ser-code <drynkin12@gmail.com>
2026-01-17 14:02:01 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+int main()
+{
+	int integer{ };
+	std::cin >> integer;
+	std::cout << "Your number: " << integer << std::endl;
+	std::cout << "My number: " << integer << std::endl;
+	return EXIT_SUCCESS;
+}
